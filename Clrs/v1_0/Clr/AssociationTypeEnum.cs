e46737b4af72ded7f7042e9f7b55c19b6870194d
Keith Williams <keith.williams@randasolutions.com>
2023-09-22 16:32:41 -0500

    Fix AssociationTypeEnum serialization issue

--- Clrs/v1_0/Clr/AssociationTypeEnum.cs
+++ Clrs/v1_0/Clr/AssociationTypeEnum.cs
@@ -1,19 +1,36 @@
 using Newtonsoft.Json.Linq;
+using OpenCredentialPublisher.Credentials.Converters.Json;
+using OpenCredentialPublisher.Credentials.Converters.Newtonsoft;
+using System.ComponentModel;
 using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace OpenCredentialPublisher.Credentials.Clrs.v1_0.Clr
 {
+    [TypeConverter(typeof(CustomEnumConverter<AssociationTypeEnum>))]
+    [JsonConverter(typeof(StringEnumConverter<AssociationTypeEnum>))]
+    [Newtonsoft.Json.JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
     public enum AssociationTypeEnum
     {
+        [EnumMember(Value = "exactMatchOf")]
         exactMatchOf = 1,
+        [EnumMember(Value = "exemplar")]
         exemplar = 2,
+        [EnumMember(Value = "hasSkillLevel")]
         hasSkillLevel = 3,
+        [EnumMember(Value = "isChildOf")]
         isChildOf = 4,
+        [EnumMember(Value = "isParentOf")]
         isParentOf = 5,
+        [EnumMember(Value = "isPartOf")]
         isPartOf = 6,
+        [EnumMember(Value = "isPeerOf")]
         isPeerOf = 7,
+        [EnumMember(Value = "isRelatedTo")]
         isRelatedTo = 8,
+        [EnumMember(Value = "precedes")]
         precedes = 9,
+        [EnumMember(Value = "replacedBy")]
         replacedBy = 10
     }
 }
