a29b620e7a09f3d269931e82986e58af8a78f2fe
Keith Williams <keith.williams@randasolutions.com>
2023-09-22 18:10:51 -0500

    added serialization for Verification type

--- Clrs/v1_0/Clr/VerificationTypeEnum.cs
+++ Clrs/v1_0/Clr/VerificationTypeEnum.cs
@@ -1,9 +1,21 @@
+using OpenCredentialPublisher.Credentials.Converters.Json;
+using OpenCredentialPublisher.Credentials.Converters.Newtonsoft;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
 namespace OpenCredentialPublisher.Credentials.Clrs.v1_0.Clr
 {
+    [TypeConverter(typeof(CustomEnumConverter<VerificationTypeEnum>))]
+    [JsonConverter(typeof(StringEnumConverter<VerificationTypeEnum>))]
+    [Newtonsoft.Json.JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
     public enum VerificationTypeEnum
     {
+        [EnumMember(Value = "Hosted")]
         Hosted = 1,
+        [EnumMember(Value = "Signed")]
         Signed = 2,
+        [EnumMember(Value = "Verification")]
         Verification = 3
     }
 
