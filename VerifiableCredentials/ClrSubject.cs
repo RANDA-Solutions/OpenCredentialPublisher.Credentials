3fff8ba6939f609c30362a6676609441effdc57d
Keith Williams <keith.williams@randasolutions.com>
2023-09-26 14:58:53 -0500

    Added missing CatchAllSubject

--- VerifiableCredentials/ClrSubject.cs
+++ VerifiableCredentials/ClrSubject.cs
@@ -1,8 +1,13 @@
-﻿using OpenCredentialPublisher.Credentials.Clrs.v1_0.Clr;
+﻿using Newtonsoft.Json;
+using OpenCredentialPublisher.Credentials.Clrs.v1_0.Clr;
+using System.Collections.Generic;
 
 namespace OpenCredentialPublisher.Credentials.VerifiableCredentials
 {
     public class ClrSubject: ClrDType, ICredentialSubject
     {
+        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore), System.Text.Json.Serialization.JsonPropertyName("type")]
+        [Newtonsoft.Json.JsonConverter(typeof(SingleOrArrayConverter<string>)), System.Text.Json.Serialization.JsonConverter(typeof(OpenCredentialPublisher.Credentials.Converters.Json.SingleOrArrayConverter<string>))]
+        public new List<string> Type { get; set; }
     }
 }
--- VerifiableCredentials/CredentialSubject.cs
+++ VerifiableCredentials/CredentialSubject.cs
@@ -1,23 +1,36 @@
 ﻿using JsonSubTypes;
 using Newtonsoft.Json;
+using OpenCredentialPublisher.Credentials.Attributes;
 using System;
+using System.Collections.Generic;
 
 namespace OpenCredentialPublisher.Credentials.VerifiableCredentials
 {
 
-    [JsonConverter(typeof(JsonSubtypes), "Type")]
+    [JsonConverter(typeof(JsonSubtypes), "type")]
     [JsonSubtypes.KnownSubType(typeof(ClrSubject), "Clr")]
     [JsonSubtypes.KnownSubType(typeof(ClrSetSubject), "ClrSet")]
+    [JsonSubtypes.KnownSubType(typeof(CatchAllSubject), "*")]
     public interface ICredentialSubject
     {
-        [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
+        [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore), System.Text.Json.Serialization.JsonPropertyName("id")]
         String Id { get; set; }
-        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore)]
-        String Type { get; set; }
+        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore), System.Text.Json.Serialization.JsonPropertyName("type")]
+        public List<string> Type { get; set; }
     }
-    public abstract class CredentialSubject: ICredentialSubject
+    public abstract class CredentialSubject : ICredentialSubject
     {
         public String Id { get; set; }
-        public String Type { get; set; }
+        [JsonProperty("type", NullValueHandling = NullValueHandling.Ignore), System.Text.Json.Serialization.JsonPropertyName("type")]
+        [Newtonsoft.Json.JsonConverter(typeof(SingleOrArrayConverter<string>)), System.Text.Json.Serialization.JsonConverter(typeof(OpenCredentialPublisher.Credentials.Converters.Json.SingleOrArrayConverter<string>))]
+        public List<string> Type { get; set; }
+    }
+
+    [JsonType("*")]
+    public class CatchAllSubject : CredentialSubject
+    {
+        [System.Text.Json.Serialization.JsonExtensionData]
+        [System.Text.Json.Serialization.JsonPropertyName("additionalProperties"), Newtonsoft.Json.JsonProperty("additionalProperties")]
+        public Dictionary<String, Object> AdditionalProperties { get; set; }
     }
 }
